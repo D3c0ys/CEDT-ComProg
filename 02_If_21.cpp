c6fa90e2254cfe9496b7dfb8e4f54e427e9be8a6
D3c0ys <viruspixel@protonmail.com>
2026-08-25 11:34:24 +0700

    Completed 02_If_21.cpp

--- 02_If_21.cpp
+++ 02_If_21.cpp
@@ -1,17 +1,24 @@
 // 02_If_21  02_If_★★_AbbrevNum
 #include <iostream>
 #include <string>
+#include <cmath>
 
 int main() {
 	std::string num;
 	std::cin >> num;
 
-	std::cout << num.length();
-
-	if(num.length()-1 % 3 == 0)
-	{
-		std::cout << (num.length()-1)/3;
+	if (num.length()>=4 && num.length()<=6 ){
+		std::cout << round(stoi(num)/pow(10,(num.length()==4?2:3)))/(num.length()==4?10:1) << "K";
 	}
-
-    return 0;
+	else if (num.length()>=7 && num.length()<=9 ){
+		num = num.substr(0,num.length()-3);
+		std::cout << round(stoi(num)/pow(10,(num.length()==4?2:3)))/(num.length()==4?10:1) << "M";
+	}
+	else if (num.length()>=10){
+		num = num.substr(0,num.length()-6);
+		std::cout << round(stoi(num)/pow(10,(num.length()==4?2:3)))/(num.length()==4?10:1) << "B";
+	}
+	else{
+		std::cout << stoi(num);
+	}
 }
